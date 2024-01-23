70865665e4a0d6df2d34bf0dee6f478d3ba47401
nihad <nihad@hodo.in>
2024-01-23 22:24:38 +0530

    add header section

---
+++ src/components/MenuItem.jsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function MenuItem({title,address,Icon}) {
+  return (
+    <Link className="hover:text-amber-500" href={address}>
+        <Icon className="text-2xl sm:hidden"/>
+        <p className="uppercase hidden sm:inline text-sm">{title }</p>
+    </Link>
+  )
+}
