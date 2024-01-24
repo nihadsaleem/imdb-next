8ffa1e46733afb74a678386a62e76cdbc5871ab6
nihad <nihad@hodo.in>
2024-01-24 22:54:39 +0530

    about us page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,10 @@
+export default function page() {
+  return (
+    <div className="max-w-6xl mx-auto p-3 space-y-4">
+        <h1 className="text-2xl font-medium text-amber-600">About</h1>
+        <p>lorem</p>
+        <p>lorem</p>
+        <p>lorem</p>
+    </div>
+  )
+}
