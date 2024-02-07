4c75ba873f3371130d004c4a0d9d1629d0ba4cb1
nihad <nihad@hodo.in>
2024-02-08 00:12:24 +0530

    trying fix

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -2,9 +2,9 @@ export default function About() {
   return (
     <div className="max-w-6xl mx-auto p-3 space-y-4">
         <h1 className="text-2xl font-medium text-amber-600">About</h1>
-        <p>lorem</p>
-        <p>lorem</p>
-        <p>lorem</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <p>Ut aliquet nulla nec enim porttitor, sit amet pretium neque condimentum.</p>
+        <p>Integer vel libero nec lacus porta auctor id et ex.</p>
     </div>
-  )
+  );
 }
