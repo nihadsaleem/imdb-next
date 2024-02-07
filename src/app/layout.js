e3b28abe87f786aeb11bf8d8e6d0c1ff8ede42e4
nihad <nihad@hodo.in>
2024-02-07 23:24:57 +0530

    search box added

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import './globals.css'
 import Header from '@/components/Header'
 import Providers from './Providers'
 import Navbar from '@/components/Navbar'
+import SearcBox from '@/components/SearcBox'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,6 +19,7 @@ export default function RootLayout({ children }) {
         <Providers>
           <Header />
           <Navbar />
+          <SearcBox/>
           {children}
         </Providers>
       </body>
