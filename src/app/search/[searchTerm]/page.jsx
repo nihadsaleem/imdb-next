9a6c8df140cf7b56c6bc03f43c9738345ad18855
nihad <nihad@hodo.in>
2024-02-08 00:06:12 +0530

    changes

--- src/app/search/[searchTerm]/page.jsx
+++ src/app/search/[searchTerm]/page.jsx
@@ -2,7 +2,7 @@ import Results from "@/components/Results";
 
 export default async function SearchPage({ params }) {
     const searchTerm = params.searchTerm;
-    const res = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&query=${searchTerm}&language=en-US&page=1&include_adult=true`);
+    const res = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&query=${searchTerm}&language=en-US&page=1&include_adult=false`);
     const data = await res.json();
     const results = data.results;
   
