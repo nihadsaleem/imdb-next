a07d03a8968eb015440cf382e4ff1b04f5583231
nihad <nihad@hodo.in>
2024-02-08 21:31:29 +0530

    space removed

--- src/app/page.js
+++ src/app/page.js
@@ -2,19 +2,8 @@ import Results from "@/components/Results";
 
 const API_KEY = process.env.API_KEY;
 
-export default async function Home({ searchParams }) {
+export default async function Home ({searchParams}) {
   const genre = searchParams.genre || 'fetchTrending';
-  // const res = await new Promise((resolve) => {
-  //   setTimeout(async () =>{
-  //     const response = await fetch(
-  //       `https://api.themoviedb.org/3${
-  //         genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`
-  //       }?api_key=${API_KEY}&language=en-US&page=1`,
-  //       { next: {revalidate:10}}
-  //     );
-  //     resolve(response);
-  //   },2000);
-  // });
   const res = await fetch(
     `https://api.themoviedb.org/3${
       genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`
@@ -26,7 +15,6 @@ export default async function Home({ searchParams }) {
     throw new Error('Failed to fetch data');
   }
 
-  // const data = await res.json();
   const results = data.results;
   return <div>
     <Results results = {results}/>
