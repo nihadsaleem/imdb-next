11de99a3c8448343e9bd5b4d52f51ede4a0f9dec
nihad <nihad@hodo.in>
2024-02-13 21:40:13 +0530

    error fix

--- next.config.js
+++ next.config.js
@@ -11,4 +11,9 @@ const nextConfig = {
     },
 }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+module.exports = {
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
+}
